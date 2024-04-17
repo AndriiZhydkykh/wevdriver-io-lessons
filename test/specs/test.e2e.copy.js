d466047965fabca7b95d3860449a1f85b6014fb5
andriizhydkykh <andry.zhidkikh@gmail.com>
2024-04-17 13:34:52 +0300

    Change some things

--- test/specs/test.e2e.copy.js
+++ test/specs/test.e2e.copy.js
@@ -32,53 +32,41 @@ describe('wdio commands', () => {
         await browser.url(`https://the-internet.herokuapp.com/login`)
         const userNameField = await $('input#username')
 
-        const userNameFieldType = await (userNameField).getAttribute('type') //save key of attribute type
+        const userNameFieldType = await userNameField.getAttribute('type') //save key of attribute type
+
         await userNameField.addValue('some text')
-        const userNameFieldValue = await (userNameField).getValue() //save value of element
+    
+        const userNameFieldValue = await userNameField.getValue() //save value of element
         console.log('userNameFieldType is ' + userNameFieldType) // print to console saved user Name Field Type
-        console.log('userNameFieldValue is ' + userNameFieldValue) // print to console saved user Name Field Value
+        console.log('userNameFieldValue is ' + userNameFieldValue) // print to console saved user Name Field Value */
     })
     it('AT3 - save text and type this text to field', async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`)
         const title = await $('div.example h2')
         const userNameField = await $('input#username')
 
-        const titleText = await (title).getText()
+        const titleText = await title.getText()
 
         console.log('title text is ' + titleText)  // should be a Login Page
 
         await userNameField.addValue(titleText)
-
-        const userNameFieldValue = await (userNameField).getValue()
-        console.log('userNameFieldValue is ' + userNameFieldValue)  // should be a Login Page
+        await browser.pause(2000)
+        await expect(userNameField).toHaveValue('Login Page') 
     })
-    it('AT4 - is Clickable', async () => {
-        await browser.url(`https://webdriver.io/`)
-        const headerNavBlogLink = await $('nav a[href="/blog"]')
-
-        let headerNavBlogLinkIsClickable = await headerNavBlogLink.isClickable()
-        console.log('is Clickable ' + headerNavBlogLinkIsClickable)  // true 
-    })
-    it('AT5 - is Displayed', async () => {
-        await browser.url(`https://webdriver.io/`)
-        const headerNavBlogLink = await $('nav a[href="/blog"]')
-        const footerGettingStartedLink = await $('footer a[href="/docs/gettingstarted"]')
-        const headerNavBlogLinkIsDisplayed = await headerNavBlogLink.isDisplayed()
-        const footerGettingStartedLinkIsDisplayed = await footerGettingStartedLink.isDisplayed()
-        console.log('footerGettingStartedLink is Displayed ' + headerNavBlogLinkIsDisplayed)  // true 
-        console.log('footerGettingStartedLink is Displayed ' + footerGettingStartedLinkIsDisplayed)  // true 
-    })
-    it('AT6 - is Displayed In Viewport', async () => {
+    it('AT4 - scrollIntoView', async () => {
         await browser.url(`https://webdriver.io/`)
         const headerNavBlogLink = await $('nav a[href="/blog"]')
         const footerGettingStartedLink = await $('footer a[href="/docs/gettingstarted"]')
         const headerNavBlogLinkIsDisplayedInViewport = await headerNavBlogLink.isDisplayedInViewport()
+
+        await footerGettingStartedLink.scrollIntoView()// scroll to start of block
+
         const footerGettingStartedLinkIsDisplayedInViewport = await footerGettingStartedLink.isDisplayedInViewport()
         console.log('headerNavBlogLink  is Displayed In Viewport ' + headerNavBlogLinkIsDisplayedInViewport)  // true 
-        console.log('footerGettingStartedLink  is Displayed In Viewport ' + footerGettingStartedLinkIsDisplayedInViewport)  // false 
+        console.log('footerGettingStartedLink  is Displayed In Viewport ' + footerGettingStartedLinkIsDisplayedInViewport)  // true 
     })
-    it('AT7 - scrollIntoView', async () => {
-        await browser.url(`https://webdriver.io/`)
+    it.only('AT5 - scrollIntoView', async () => {
+        await browser.url(`https://docs.cypress.io/`)
         const headerNavBlogLink = await $('nav a[href="/blog"]')
         const footerGettingStartedLink = await $('footer a[href="/docs/gettingstarted"]')
         const headerNavBlogLinkIsDisplayedInViewport = await headerNavBlogLink.isDisplayedInViewport()
